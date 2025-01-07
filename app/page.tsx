490bbafeeea6043396c80b230d75512e80fca276
David Howard <david.howard1100@gmail.com>
2025-01-07 16:17:21 -0500

    Add loading indicator

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,26 @@ import { WeatherResponse } from "@/utils/types";
 
 async function getWeatherForZipCode(
   zipCode: string,
-  setWeather: (weather: WeatherResponse | null) => void
+  setWeather: (weather: WeatherResponse | null) => void,
+  setLoading: (loading: boolean) => void
 ) {
+  setLoading(true);
+
   if (zipCode === "") {
     console.error("Zip code not set");
+    setLoading(false);
     return;
   }
 
   if (zipCode.length !== 5) {
     console.error("Zip code must be 5 digits");
+    setLoading(false);
     return;
   }
 
   if (isNaN(parseInt(zipCode))) {
     console.error("Zip code must be a number");
+    setLoading(false);
     return;
   }
 
@@ -28,6 +34,7 @@ async function getWeatherForZipCode(
   if (!request.ok) {
     console.error("Weather API request failed");
     setWeather(null);
+    setLoading(false);
     return;
   }
 
@@ -37,6 +44,8 @@ async function getWeatherForZipCode(
   } catch (error) {
     console.error("Failed to parse JSON response:", error);
     setWeather(null);
+  } finally {
+    setLoading(false);
   }
 }
 
@@ -52,6 +61,7 @@ export default function Home() {
   const [zipCode, setZipCode] = useState("");
   const [weather, setWeather] = useState<WeatherResponse | null>(null);
   const [isCelcius, setIsCelcius] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -69,10 +79,21 @@ export default function Home() {
               onChange={(e) => setZipCode(e.target.value)}
             />
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm"
-              onClick={() => getWeatherForZipCode(zipCode, setWeather)}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm flex items-center justify-center"
+              onClick={() => getWeatherForZipCode(zipCode, setWeather, setLoading)}
+              disabled={loading}
             >
-              Get Weather
+              {loading ? (
+                <>
+                  <svg className="animate-spin h-5 w-5 mr-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
+                  </svg>
+                  Loading...
+                </>
+              ) : (
+                "Get Weather"
+              )}
             </button>
           </div>
           {weather && (
