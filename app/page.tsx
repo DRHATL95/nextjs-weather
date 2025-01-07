68417bf7d27e829cdbccf61e964156e773726f85
David Howard <david.howard1100@gmail.com>
2025-01-07 02:24:01 -0500

    Add functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,153 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+type WeatherResponse = {
+  location: LocationResponse;
+  current: CurrentResponse;
+};
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+type LocationResponse = {
+  name: string;
+  region: string;
+  country: string;
+  lat: number;
+  lon: number;
+  tz_id: string;
+  localtime_epoch: number;
+  localtime: string;
+};
+
+type CurrentResponse = {
+  last_updated_epoch: number;
+  last_updated: string;
+  temp_c: number;
+  temp_f: number;
+  is_day: number;
+  condition: {
+    text: string;
+    icon: string;
+    code: number;
+  };
+  wind_mph: number;
+  wind_kph: number;
+  wind_degree: number;
+  wind_dir: string;
+  pressure_mb: number;
+  pressure_in: number;
+  precip_mm: number;
+  precip_in: number;
+  humidity: number;
+  cloud: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  vis_km: number;
+  vis_miles: number;
+  uv: number;
+  gust_mph: number;
+  gust_kph: number;
+};
+
+const WEATHER_API_BASE = "http://api.weatherapi.com/v1";
+
+async function getWeatherForZipCode(
+  zipCode: string,
+  setWeather: (weather: WeatherResponse) => void
+) {
+  console.log("In here..");
+
+  if (zipCode === "") {
+    console.error("Zip code not set");
+    return;
+  }
+
+  if (zipCode.length !== 5) {
+    console.error("Zip code must be 5 digits");
+    return;
+  }
+
+  if (process.env.NEXT_PUBLIC_WEATHER_API_KEY === undefined) {
+    console.error("WEATHER_API_KEY not set");
+    return;
+  }
+
+  if (isNaN(parseInt(zipCode))) {
+    console.error("Zip code must be a number");
+    return;
+  }
+
+  const url = `${WEATHER_API_BASE}/current.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${zipCode}`;
+  console.log(url);
+  const request = await fetch(url);
+  const response: WeatherResponse = await request.json();
+
+  console.log(response);
+  setWeather(response);
+}
+
+function convertCelciusToFahrenheit(celcius: number | undefined): number {
+  if (celcius === undefined) {
+    return 0;
+  }
+
+  return Math.round((celcius * 9) / 5 + 32);
+}
+
+export default function Home() {
+  const [zipCode, setZipCode] = useState("");
+  const [weather, setWeather] = useState<WeatherResponse | null>(null);
+  const [isCelcius, setIsCelcius] = useState(true);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 items-center w-full max-w-md">
+        <div className="text-center">
+          <h1 className="text-3xl font-bold">Weather App</h1>
+          <p>Enter a zip code to get the weather</p>
+        </div>
+        <div className="flex flex-col gap-4 items-center w-full">
+          <div className="flex gap-4 items-center w-full">
+            <input
+              className="flex-1 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder="Enter Zip Code"
+              onChange={(e) => setZipCode(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm"
+              onClick={() => getWeatherForZipCode(zipCode, setWeather)}
+            >
+              Get Weather
+            </button>
+          </div>
+          <div className="flex flex-col gap-4 items-center border border-gray-300 p-4 rounded w-full">
+            <p>{weather?.location.name}, {weather?.location.region}</p>
+            {weather && weather.current.condition ? (
+              <div className="flex flex-col items-center">
+                <Image
+                  src={`https:${weather?.current.condition.icon}`}
+                  alt={weather?.current.condition.text}
+                  width={50}
+                  height={50}
+                />
+                <p>{weather?.current.condition.text}</p>
+              </div>
+            ) : (
+              ""
+            )}
+            <p
+              className="cursor-pointer"
+              onClick={() => setIsCelcius(!isCelcius)}
+            >
+              {isCelcius
+                ? weather?.current.temp_c + " °C"
+                : convertCelciusToFahrenheit(weather?.current.temp_c) + " °F"}
+            </p>
+            <p>Wind: {weather?.current.wind_kph} kph</p>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
